79c189aef2ee6e804d1171243939c8ddf522964b
Nicholas Donahue <nicholas.donahue@mynbps.org>
2021-02-05 04:56:02 +0000

    wrote the runBoard method

--- Game.java
+++ Game.java
@@ -2,29 +2,101 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Game {
+
   public Scanner Input = new Scanner(System.in);
   private ArrayList<Player> Profiles = new ArrayList<Player>();
+  private ArrayList<Board> Boards = new ArrayList<Board>();
 
   public Game() {
     clearScreen();
+    // create the first player profile.
     System.out.println("WELCOME TO TIC-TAC-TOE!");
     pause(2500);
     System.out.println("What is your name?\n");
+    
     String playerName = Input.nextLine();
-    if playerName.equals("") {
+    if (playerName.equals("")) {
       Profiles.add(new Player());
     } else {
       Profiles.add(new Player(playerName));
     }
+
+    runMenu();
   }
 
-  public void runMenu() {
+  private void runMenu() {
     clearScreen();
     System.out.println("WELCOME TO TIC-TAC-TOE!\n\nEnter an option's number: (ex. 1, 2, ...)");
-    pause(2500);
     System.out.println("1. Play");
     System.out.println("2. Create a profile");
     System.out.println("3. View all profiles");
+
+    String menuChoice = Input.nextLine();
+
+    if (menuChoice.equals("1")) {  // send to the Play method
+      runBoard();
+    } else if (menuChoice.equals("2")) { // create a new profile
+      runProfileCreation();
+    } else if (menuChoice.equals("3")) { // view all existing profiles
+      runViewProfiles();
+    } else { // if no option is chosen, reset
+      runMenu();
+    }
+
+  }
+
+  private void runBoard() {
+    if (Profiles.size() == 1) { // check if only one profile exists
+      clearScreen();
+      System.out.println("What is the name of your opponent?\n");
+
+      String playerName = Input.nextLine();
+      if (playerName.equals("")) {
+        Profiles.add(new Player());
+      } else {
+        Profiles.add(new Player(playerName));
+      }     
+    }
+
+    // select profiles for the match
+    clearScreen();
+    System.out.println("Match Creation\n");
+    System.out.println("Enter an profile's number: (ex. 1, 2, ...)");
+
+    for (int i = 0; i < Profiles.size(); i++) {
+      System.out.println((i+1) + ". " + Profiles.get(i).getName());
+    }
+
+    System.out.println("8. return to the main menu\n");
+    System.out.println("Select Player 1: (ex. 1, 2, ...)\n");
+    int matchCoice1 = Input.nextInt();
+    int matchChoice2;
+
+    if (matchCoice1 == 8) { // return to the menu
+      runMenu();
+    } else if (matchCoice1 >= 1 && matchCoice1 <= (Profiles.size() + 1)) {
+      System.out.println("Select Player 2: (ex. 1, 2, ...)\n");
+      matchChoice2 = Input.nextInt(); 
+      if (matchCoice1 == 8) { // return to the menu
+        runMenu();
+      } else if (matchCoice1 >= 1 && matchCoice1 <= (Profiles.size() + 1)) {
+        //
+      } else {
+        runBoard();
+      }
+    } else {
+      runBoard();
+    }
+
+    Board gameBoard = new Board(Profiles.get(matchCoice1 - 1), Profiles.get(matchCoice2 - 1));
+  }
+
+  private void runProfileCreation() {
+    runMenu();
+  }
+
+  private void runViewProfiles() {
+    runMenu();
   }
 
   public void Run() {
--- Player.java
+++ Player.java
@@ -1,9 +1,31 @@
 class Player {
-  public Player() {
+  private int PlayerScore;
+  private String Name;
 
+  public Player() {
+    Name = "Unknown Player Name";
+    PlayerScore = 0;
   }
   
-  public Player(String Name) {
+  public Player(String nameV) {
+    Name = nameV;
+    PlayerScore = 0;
+  }
 
+  // getters
+  public String getName() { return Name; }
+  public int getScore() { return PlayerScore; }
+
+  // setters
+  public void updateName(String nameV) {
+    Name = nameV;
+  }
+
+  public void setScore(int scoreV) {
+    PlayerScore = scoreV;
+  }
+
+  public void incrementScore(int scoreV) {
+    PlayerScore = PlayerScore + scoreV;
   }
 }
